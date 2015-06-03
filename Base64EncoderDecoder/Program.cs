d8adbaeb7f4c8fff4038b289b09e82f7fe20c73f
crayoncode <crayoncode@163.com>
2015-06-03 16:22:08 +0800

    Base64 编码，解码

---
+++ Base64EncoderDecoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64EncoderDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //编码
+            byte[] bytes = Encoding.Default.GetBytes("你好");
+            Console.WriteLine(Convert.ToBase64String(bytes));
+            //解码
+            byte[] outputb = Convert.FromBase64String("ztKwrsTj");
+            string orgStr = Encoding.Default.GetString(outputb);
+            Console.WriteLine(orgStr);
+            Console.ReadKey();
+        }
+    }
+}
